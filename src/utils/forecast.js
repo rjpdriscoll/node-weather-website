344546b2fc103f18cc5be886fa0afd82cff1ddf0
Ryan Driscoll <rjpdriscoll@Ryans-MacBook-Air.local>
2021-02-24 14:06:11 -0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=5a6a018bcf2506b2b9228c314b089c4f&query=${latitude},${longitude}&units=f`;
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to the weather service.', undefined);
+        } else if (body.error) {
+            callback('Unable to retrieve weather for provided query.', undefined);
+        } else {
+            const curr = body.current;
+            callback(undefined, `${curr.weather_descriptions[0]}. It is currently ${curr.temperature} degrees out. It feels like ${curr.feelslike} degrees out.`);
+        }
+    });
+};
+
+module.exports = forecast;
